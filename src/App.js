0958934c47d3b9fb5b201978e021f8f0d60ca115
AJ <42003122+code7885@users.noreply.github.com>
2017-02-16 12:25:57 +0100

    initialize the project and folder structure, as well as build configuration with webpack , babel, css and optimizer

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import logo from './img/logo.svg';
+import './sass/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+
+          <h2>Welcome Super Simple Chat Application</h2>
+        </div>
+        <p className="App-intro">
+          Lets start.
+        </p>
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var webpack = require("webpack");
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      // path: path.join(__dirname, 'dist'),
+        path: './public/js',
+        filename: 'bundle.js',
+        publicPath: '/js/'
+    },
+    devtool: 'source-map',
+    // devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        inline: true,
+        contentBase: "./public/",
+        port: 3000
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: ["babel-loader"],
+                query: {
+                    presets: ["latest", "stage-0", "react"]
+                }
+            },
+            {
+                test: /\.json$/,
+                exclude: /(node_modules)/,
+                loader: "json-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+            }
+        ]
+    },
+    plugins: [
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {discardComments: {removeAll: true}},
+            canPrint: true
+        })
+    ]
+}
